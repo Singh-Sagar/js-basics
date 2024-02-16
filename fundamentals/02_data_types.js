d5b3513e2d5ba4164140f2ba55a3efbaa127c84f
Sagar Singh <126241039+Singh-Sagar@users.noreply.github.com>
2024-02-16 17:10:33 +0000

    conversion operations

---
+++ fundamentals/02_data_types.js
@@ -0,0 +1,7 @@
+// undefined
+let state;
+console.log(typeof(state)); // returns undefined. 
+
+// null=> object
+let x = null;
+console.log(typeof(x));
---
+++ fundamentals/03_conversionOperation.js
@@ -0,0 +1,13 @@
+
+// A bug or specific design of javascript. 
+
+let x = "apple";
+console.log(typeof x);
+
+numX = Number(x);
+console.log(typeof numX);
+console.log(numX);
+
+// Num(undefined) => NaN
+// Num(null) => 0;
+// Num(true) => 1;

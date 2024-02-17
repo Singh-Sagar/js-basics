928f82ffa558b7870172f7de1ce28fa9349188da
Sagar Singh <126241039+Singh-Sagar@users.noreply.github.com>
2024-02-17 04:06:47 +0000

    prefix postfix

---
+++ fundamentals/05_prefix_postfix.js
@@ -0,0 +1,17 @@
+
+// POSTFIX OPERATOR
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected : x:4 y:4
+// Actual: x:4 y:3
+
+
+
+// PREFIX OPERATOR
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected: a:4 b:4
